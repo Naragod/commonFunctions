077bde8d07a3c0f173a72898b65f6c343a576dfc
Naragod <mateo.naranjo@mail.utoronto.ca>
2021-05-12 15:56:06 -0400

    added asynchronous parallel execution and update the tests.

--- javascript/promises/helper.js
+++ javascript/promises/helper.js
@@ -1,26 +1,38 @@
+let starter = Promise.resolve(null);
+
 /**
  *
  * @param {Promise[]} promises
  * @returns an array of promises in the order in which they finish their execution
  * not in the order in which they are passed in the array
  */
-const parallel = async (promises) => {
+const parallelSync = async (promises) => {
   return new Promise((resolve, _reject) => {
     const results = [];
-    let starter = Promise.resolve(null);
     promises
-      .map(async (promise) => {
-        // add the result of promise's execution as they finish to the results array
-        return promise.then((res) => results.push(res)).catch((err) => console.log("AAAA:", err));
-      })
-      .reduce((prev, curr) => {
-        // wait for the returned promises to exectute before returning the results array.
-        // otherwise it will return an empty array when we resolve.
-        return prev.then(() => curr.then((res) => res));
-      }, starter)
+      // add the result of promise's execution as they finish to the results array
+      .map((promise) => promise.then((res) => results.push(res)).catch((err) => console.log("AAAA:", err)))
+      // wait for the returned promises to exectute before returning the results array.
+      // otherwise it will return an empty array when we resolve.
+      .reduce((prev, curr) => prev.then(() => curr.then((res) => res)), starter)
       // return result array. Have to do it inside the then statement to ensure that the results array is populated.
       .then(() => resolve(results));
   });
+};
+
+const parallelAsync = async (promises, callback) => {
+  return new Promise((resolve, _reject) => {
+    promises
+      .map((promise) => promise.then((res) => callback(res)).catch((err) => console.log("BBBB:", err)))
+      // care about awaiting for the promises to finish in order to continue execution. Promise result can be ignored.
+      .reduce((prev, curr) => prev.then(() => curr.then(), starter))
+      .then((res) => resolve(res));
+  });
+};
+
+const parallel = (promises, callback) => {
+  if (callback) return parallelAsync(promises, callback);
+  return parallelSync(promises);
 };
 
 module.exports = { parallel };
--- javascript/promises/test.js
+++ javascript/promises/test.js
@@ -13,13 +13,34 @@ const task = function (taskNum, seconds, negativeScenario) {
 // copied test code.
 // modified
 const run = async function () {
+  await timeTest(async () => {
+    let results = [];
+    // asyncronous result
+    await parallel(getPromises(), (promiseResult) => results.push(promiseResult));
+    console.log(results);
+  });
+
+  await timeTest(async () => {
+    // synchronous result
+    let results = await parallel(getPromises());
+    console.log(results);
+  });
+};
+
+const getPromises = () => {
   let t1 = task(1, 6, false);
   let t2 = task(2, 5, true);
   let t3 = task(3, 3, false);
   let t4 = task(4, 4, false);
-  const results = await parallel([t1, t2, t3, t4]);
-  console.log(results);
-  console.log("Finished ~~~~~~~~~~~~~~~~~");
+  return [t1, t2, t3, t4];
+};
+
+const timeTest = async (callback) => {
+  const start = Date.now();
+  await callback();
+  const end = Date.now();
+  const timeDiff = end - start;
+  console.log(`~~~~~~~~~~~~~~~~~ Finished in ${timeDiff} ms ~~~~~~~~~~~~~~~~~`);
 };
 
 (async function () {
